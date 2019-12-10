8ed1a6be2b7625916970dcbaaee85ef83d64d9c2
Will Jordan <will@code.org>
2019-12-10 15:36:52 -0800

    Send CloudWatch and Firehose events in async buffered batches Add Cdo::Buffer to handle asynchronous buffering and batching.

---
+++ lib/cdo/buffer.rb
@@ -0,0 +1,175 @@
+require 'concurrent/scheduled_task'
+require 'honeybadger/ruby'
+
+module Cdo
+  # Abstract class to handle asynchronous-buffering and periodic-flushing using a thread pool.
+  # This class is content-agnostic, buffering event objects in memory through #buffer(event, size).
+  # Subclasses implement #flush(events), which will be called periodically as the buffer is flushed in batches.
+  #
+  # Because events are stored in memory, the buffer can synchronously flush when the Ruby process exits.
+  class Buffer
+    # @param [Integer] batch_events   Maximum number of events in a buffered batch.
+    # @param [Integer] batch_size     Maximum total payload 'size', based on the size parameter passed to #buffer,
+    #                                 in a buffered batch.
+    # @param [Float] max_interval     Seconds after the first buffered item before a flush will occur.
+    # @param [Float] min_interval     Seconds after the previous flush before a flush will occur.
+    #                                 Useful for rate-throttling.
+    # @param [Float] wait_at_exit     Seconds to wait at exit for flushing to complete.
+    def initialize(
+      batch_events: 2 << 32,
+      batch_size:   2 << 32,
+      max_interval: Float::INFINITY,
+      min_interval: 0.0,
+      wait_at_exit: nil
+    )
+      @batch_events = batch_events
+      @batch_size   = batch_size
+      @max_interval = max_interval
+      @min_interval = min_interval
+
+      @scheduled_task = nil
+      @buffer = Batch.new
+
+      @read, @write = UNIXSocket.pair(:DGRAM)
+      @buffer_thread = Thread.new(&method(:buffer_loop))
+
+      if wait_at_exit
+        at_exit {close(wait_at_exit) unless @write.closed?}
+      end
+    end
+
+    # Implement in subclass.
+    # @param [Array<Object>] events
+    def flush(events)
+    end
+
+    # Add an event to the buffer.
+    # @raise [ArgumentError] when the event exceeds batch size
+    # @raise [IOError, Errno::EPIPE] when the Buffer is closed
+    # @param [Object] event
+    # @param [Integer] size
+    def buffer(event, size = nil)
+      size ||= event.is_a?(String) ? event.bytesize : 1
+      raise ArgumentError, 'Event exceeds batch size' if size > @batch_size
+      if @buffer_thread.alive?
+        @buffer << Batch::Item.new(event, size, now)
+        schedule_flush
+      else
+        @write << Marshal.dump([event, size])
+      end
+    end
+
+    # Close the buffer, flushing existing buffered events.
+    # @param [Float] timeout seconds to wait for buffered events to finish flushing.
+    def close(timeout = Float::INFINITY)
+      if @buffer_thread.alive?
+        @read.shutdown
+        start = now
+        @buffer_thread.join(timeout)
+        flush!(start - now + timeout)
+      end
+      @write.close
+      @read.close
+    end
+
+    # Flush existing buffered events.
+    # @param [Float] timeout seconds to wait for buffered events to finish flushing.
+    def flush!(timeout = Float::INFINITY)
+      start = now
+      until (wait = start - now + timeout) < 0 || @buffer.empty?
+        wait = nil if wait.infinite?
+        schedule_flush(true).value(wait)
+      end
+    end
+
+    private
+
+    def now
+      Concurrent.monotonic_time
+    end
+
+    # Buffer events serialized through a socket.
+    # The loop will exit when #shutdown is called on the socket.
+    def buffer_loop
+      len = @read.getsockopt(:SOCKET, :RCVBUF).int
+      until (msg = @read.recv(len)).empty?
+        buffer(Marshal.load(msg))
+      end
+    end
+
+    # Schedule a flush in the future when the next batch is ready.
+    # @param [Boolean] force flush batch even if not full.
+    # @return [Concurrent::ScheduledTask]
+    def schedule_flush(force = false)
+      delay = batch_ready(force)
+      unless @scheduled_task&.reschedule(delay) || @scheduled_task&.pending?
+        @scheduled_task = Concurrent::ScheduledTask.execute(delay) do
+          flush_batch
+          schedule_flush
+        end
+      end
+      @scheduled_task
+    end
+
+    # Determine when the next batch of events is ready to be flushed.
+    # @param [Boolean] force flush batch even if not full.
+    # @return [Float] Seconds until the next batch can be flushed.
+    def batch_ready(force)
+      # Wait until max_interval has passed since the earliest event to flush a non-full batch.
+      wait = @max_interval - (now - @buffer.earliest)
+
+      # Flush now if the batch is full or when force flushing.
+      wait = 0.0 if force ||
+        @buffer.size >= @batch_size ||
+        @buffer.length >= @batch_events
+
+      # Flush later if last flush was more recent than min_interval.
+      [0.0, wait, @min_interval - (now - @last_flush.to_f)].max.to_f
+    end
+
+    # Flush a batch of events from the buffer.
+    def flush_batch
+      @last_flush = now
+      flush(take_batch.map(&:event))
+    rescue => e
+      Honeybadger.notify(e)
+      raise
+    end
+
+    # Take a single batch of events from the buffer.
+    def take_batch
+      batch = Batch.new
+      batch << @buffer.shift until
+        @buffer.empty? ||
+          batch.length >= @batch_events ||
+          (batch.size + @buffer.first.size) > @batch_size
+      batch
+    end
+
+    # Helper class to track total size and earliest element in a batch of events.
+    class Batch < Array
+      Item = Struct.new(:event, :size, :created_at)
+
+      attr_reader :size
+
+      def initialize
+        super
+        @size = 0
+      end
+
+      # @param [Item] item
+      def <<(item)
+        super.tap {@size += item.size}
+      end
+
+      # @return [Item]
+      def shift
+        super.tap {|item| @size -= item.size}
+      end
+
+      def earliest
+        first&.created_at || Float::INFINITY
+      end
+    end
+  end
+end
---
+++ lib/test/cdo/test_buffer.rb
@@ -0,0 +1,121 @@
+require_relative '../test_helper'
+require 'cdo/buffer'
+
+class BufferTest < Minitest::Test
+  class TestBuffer < Cdo::Buffer
+    def flushed
+      (@flushed ||= [])
+    end
+
+    def flush(events)
+      raise "EMPTY" if events.empty?
+      flushed.push(events)
+    end
+
+    def flushes
+      flushed.length
+    end
+  end
+
+  def test_batch_events
+    b = TestBuffer.new(batch_events: 2)
+    7.times {b.buffer('foo')}
+    b.close
+    assert_equal 4, b.flushes
+  end
+
+  def test_batch_size_given_small_data_should_succeed
+    b = TestBuffer.new(batch_size: 4)
+    7.times {b.buffer('HI')}
+    b.close
+    assert_equal 4, b.flushes
+  end
+
+  def test_batch_size_given_too_large_string_should_raise_exception
+    test_string = 'HI'
+    assert_raises(ArgumentError) do
+      b = TestBuffer.new(batch_size: test_string.bytesize - 1)
+      b.buffer(test_string)
+    end
+  end
+
+  def test_batch_size_given_too_large_object_should_raise_exception
+    max_size = 5
+    assert_raises(ArgumentError) do
+      b = TestBuffer.new(batch_size: max_size)
+      b.buffer([], max_size + 1)
+    end
+  end
+
+  def test_batch_interval
+    b = TestBuffer.new(max_interval: 0.1)
+    7.times {b.buffer('bar')}
+    assert_equal 0, b.flushes
+    sleep 0.2
+    assert_equal 1, b.flushes
+    b.flush!
+    assert_equal 1, b.flushes
+  end
+
+  def test_errors_after_close
+    b = ReBuffer.new
+    b.buffer 'foo'
+    b.close
+    assert_equal 1, b.flushes
+    assert_instance_of Errno::EPIPE, b.errors.first
+    assert_raises(IOError) do
+      b.buffer 'foo'
+    end
+  end
+
+  class ReBuffer < TestBuffer
+    attr_reader :errors
+    # Re-buffer events endlessly until an error is raised when the buffer is closed.
+    def flush(events)
+      super
+      events.map(&method(:buffer))
+    rescue => e
+      (@errors ||= []) << e
+      raise
+    end
+  end
+
+  def test_fork
+    b = TestBuffer.new(batch_events: 1)
+    b.buffer 'foo'
+    pid = fork do
+      b.buffer 'bar'
+    end
+    Process.wait(pid)
+    b.buffer 'foo'
+    b.flush!
+    assert_equal 3, b.flushes
+  end
+
+  def test_min_interval
+    b = TestBuffer.new(batch_events: 1, min_interval: 1.second.to_i)
+    start = Concurrent.monotonic_time
+    4.times {b.buffer('foo')}
+    b.flush!
+    finish = Concurrent.monotonic_time
+    assert_equal 4, b.flushes
+    time_taken = finish - start
+    # Given a min_interval of 1 second and 4 flushes, assert the flush! takes 3-4 seconds.
+    assert_operator time_taken, :>, 3
+    assert_operator time_taken, :<, 4
+  end
+
+  def test_min_interval_no_flush
+    b = TestBuffer.new(batch_events: 1, min_interval: 0.1)
+    4.times {b.buffer('foo')}
+    sleep 0.05
+    assert_equal 1, b.flushes
+    b.buffer('foo')
+    sleep 0.3
+    assert_equal 4, b.flushes
+    sleep 0.1
+    assert_equal 5, b.flushes
+    sleep 0.1
+    assert_equal 5, b.flushes
+  end
+end
