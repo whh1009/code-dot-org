abc5ea4ba34072acbca4a8d0ab64876a6b4bf7e5
Winter Dong <winter@code.org>
2020-01-17 12:34:28 -0800

    Revert "Reorder eager load paths"
    This reverts commit b835799db201ef5a3448d02d1e67af2abbf58e30.

--- dashboard/config/application.rb
+++ dashboard/config/application.rb
@@ -131,11 +131,10 @@ module Dashboard
       video-js/*.css
     )
     config.autoload_paths << Rails.root.join('lib')
-
-    config.eager_load_paths << Rails.root.join('lib', 'queries')
     config.eager_load_paths << Rails.root.join('lib', 'api')
     config.eager_load_paths << Rails.root.join('lib', 'pd')
     config.eager_load_paths << Rails.root.join('lib', 'policies')
+    config.eager_load_paths << Rails.root.join('lib', 'queries')
     config.eager_load_paths << Rails.root.join('lib', 'services')
     config.eager_load_paths << Rails.root.join('app', 'models', 'experiments')
     config.eager_load_paths << Rails.root.join('app', 'models', 'levels')
