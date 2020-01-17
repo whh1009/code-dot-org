5bd4947e48ec38fbe3323e3715f5136f06c24fae
Winter Dong <winter@code.org>
2020-01-17 12:07:01 -0800

    Comment out Queries reference

--- dashboard/lib/queries/school_info.rb
+++ dashboard/lib/queries/school_info.rb
@@ -1,7 +1,7 @@
 module Queries
   class SchoolInfo
     def self.last_complete(user)
-      Queries::UserSchoolInfo.last_complete(user)&.school_info
+      #Queries::UserSchoolInfo.last_complete(user)&.school_info
     end
   end
 end
