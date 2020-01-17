332cac2077269b76a6c38cc4fb6fdf50b9811040
Winter Dong <winter@code.org>
2020-01-17 11:46:36 -0800

    Revert query object files

--- dashboard/lib/queries/school_info.rb
+++ dashboard/lib/queries/school_info.rb
@@ -1,7 +1,5 @@
-module Queries
-  class SchoolInfo
-    def self.last_complete(user)
-      UserSchoolInfo.last_complete(user)&.school_info
-    end
+class Queries::SchoolInfo
+  def self.last_complete(user)
+    Queries::UserSchoolInfo.last_complete(user)&.school_info
   end
 end
--- dashboard/lib/queries/user_school_info.rb
+++ dashboard/lib/queries/user_school_info.rb
@@ -1,12 +1,10 @@
-module Queries
-  class UserSchoolInfo
-    def self.last_complete(user)
-      user.
-        user_school_infos.
-        includes(:school_info).
-        select {|usi| usi.school_info.complete?}.
-        sort_by(&:created_at).
-        last
-    end
+class Queries::UserSchoolInfo
+  def self.last_complete(user)
+    user.
+      user_school_infos.
+      includes(:school_info).
+      select {|usi| usi.school_info.complete?}.
+      sort_by(&:created_at).
+      last
   end
 end
