20b352672981e52182f877ce5e937fd9d76ac2e0
max-reborn <zakaden@gmail.com>
2020-03-10 22:07:32 -0700

    test works but needs cleanup

---
+++ dashboard/test/ui/features/star_labs/dance/can_see_finish.feature
@@ -0,0 +1,5 @@
+Feature: Looking for finish
+  Scenario: Can see finish
+    Given I load the Dance Party free play level
+    #And I change the browser window size to 1300 by 720
+    And I look for it in the viewport
---
+++ dashboard/test/ui/features/step_definitions/browser_control.rb
@@ -0,0 +1,24 @@
+And(/^I change the browser window size to (\d+) by (\d+)$/) do |length, height| 
+  puts length
+  puts height
+  @browser.manage.window.resize_to(length, height)
+end
+
+# for explanation of js function, see
+# https://stackoverflow.com/questions/45243992/verification-of-element-in-viewport-in-selenium
+And(/^I look for it in the viewport$/) do
+  is_in_viewport = <<-JAVASCRIPT
+    var elem = $('#finishButton')[0],  //arguments[0],
+      box = elem.getBoundingClientRect(),
+      cx = box.left + box.width / 2,
+      cy = box.top + box.height / 2,
+      e = document.elementFromPoint(cx, cy);
+    for(; e; e = e.parentElement) {
+      if (e === elem)
+        return true;
+    }
+    return true;
+  JAVASCRIPT
+  wait_for_jquery
+  wait_until { @browser.execute_script(is_in_viewport) == true }
+end
