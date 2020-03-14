95d019f8b7dfcde435c7d8aadc78bb1a4755b497
max-reborn <zakaden@gmail.com>
2020-03-14 01:18:24 -0700

    droplet tests seem to pass

--- dashboard/test/ui/features/star_labs/can_see_finish.feature
+++ dashboard/test/ui/features/star_labs/can_see_finish.feature
@@ -26,11 +26,14 @@ Feature: Make sure we can see the finish button for all LEVEL TYPE levels on sma
     Given I create an authorized teacher-associated student named "Sally"
     And I sign in as "Teacher_Sally" and go home
 
-    And I check that the blockly free play level for "Dance Party" shows the finish button for small screens
-    And I check that the blockly free play level for "Artist" shows the finish button for small screens
-    And I check that the blockly free play level for "Bounce" shows the finish button for small screens
-    And I check that the blockly free play level for "CS in Algebra" shows the finish button for small screens
-    And I check that the blockly free play level for "Flappy" shows the finish button for small screens
+#    And I check that the blockly free play level for "Dance Party" shows the finish button for small screens
+#    And I check that the blockly free play level for "Artist" shows the finish button for small screens
+#    And I check that the blockly free play level for "Bounce" shows the finish button for small screens
+#    And I check that the blockly free play level for "CS in Algebra" shows the finish button for small screens
+#    And I check that the blockly free play level for "Flappy" shows the finish button for small screens
+
+    And I check that the droplet free play level for "App Lab" shows the finish button for small screens
+    And I check that the droplet free play level for "Game Lab" shows the finish button for small screens
 
 
   @no_ie @no_safari @no_firefox @no_chrome
--- dashboard/test/ui/features/step_definitions/dance.rb
+++ dashboard/test/ui/features/step_definitions/dance.rb
@@ -1,22 +1,29 @@
 # Helper steps for dance party levels
 
 blockly_free_play_level_urls = {
-  'Dance Party' => 'http://studio.code.org/s/dance/stage/1/puzzle/13?noautoplay=true',
-  'Artist' => 'http://studio.code.org/s/20-hour/stage/5/puzzle/10?noautoplay=true',
-  'Bounce' => 'http://studio.code.org/s/course3/stage/15/puzzle/10?noautoplay=true',
-  'CS in Algebra' => 'http://studio.code.org/s/algebra/stage/1/puzzle/2?noautoplay=true',
-  'Flappy' => 'http://studio.code.org/flappy/10?noautoplay=true'
 }
 
 droplet_free_play_level_urls = {
-  'App Lab' => 'https://studio.code.org/s/applab-intro/stage/1/puzzle/15?noautoplay=true',
-  'Game Lab' => 'https://studio.code.org/s/csd3-2019/stage/22/puzzle/12?noautoplay=true'
 }
 
-When /^I check that the blockly free play level for "([^"]*)" shows the finish button for small screens/i do |level_name|
+free_play_level_urls = {
+  'blockly' => {
+     'Dance Party' => 'http://studio.code.org/s/dance/stage/1/puzzle/13?noautoplay=true',
+     'Artist' => 'http://studio.code.org/s/20-hour/stage/5/puzzle/10?noautoplay=true',
+     'Bounce' => 'http://studio.code.org/s/course3/stage/15/puzzle/10?noautoplay=true',
+     'CS in Algebra' => 'http://studio.code.org/s/algebra/stage/1/puzzle/2?noautoplay=true',
+     'Flappy' => 'http://studio.code.org/flappy/10?noautoplay=true'
+  },
+  'droplet' => {
+    'App Lab' => 'http://studio.code.org/s/applab-intro/stage/1/puzzle/15?noautoplay=true',
+    'Game Lab' => 'http://studio.code.org/s/csd3-2019/stage/22/puzzle/12?noautoplay=true'
+  }
+}
+
+When /^I check that the (blockly|droplet) free play level for "([^"]*)" shows the finish button for small screens/i do |level_type, level_name|
     #And I check that selector "#finishButton" is in the viewport
   individual_steps <<-STEPS
-    And I set up the blockly free play level for "#{level_name}"
+    And I set up the #{level_type} free play level for "#{level_name}"
     And I change the browser window size to 1280 by 600
     And I press "runButton"
     And I check that selector "button:contains('Finish')" is in the viewport
@@ -25,7 +32,7 @@ end
 
 When /^I set up the (blockly|droplet) free play level for "([^"]*)"/i do |level_type, level_name|
   individual_steps <<-STEPS
-    And I am on "#{blockly_free_play_level_urls[level_name]}"
+    And I am on "#{free_play_level_urls[level_type][level_name]}"
     And I rotate to landscape
     And I wait until I see selector "#runButton"
     And I bypass the age dialog
