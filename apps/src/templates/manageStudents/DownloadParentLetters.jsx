fc96256dcd74e83be96b8d689a1c13df0775f440
Clare Constantine <clare@code.org>
2020-04-03 15:58:05 -0700

    i18n text

--- apps/src/templates/manageStudents/DownloadParentLetters.jsx
+++ apps/src/templates/manageStudents/DownloadParentLetters.jsx
@@ -5,6 +5,7 @@ import ReactTooltip from 'react-tooltip';
 import BaseDialog from '@cdo/apps/templates/BaseDialog';
 import DialogFooter from '@cdo/apps/templates/teacherDashboard/DialogFooter';
 import {SectionLoginType} from '@cdo/apps/util/sharedConstants';
+import i18n from '@cdo/locale';
 
 const INDIVIDUAL = 'individual';
 
@@ -69,7 +70,7 @@ export default class DownloadParentLetters extends Component {
             __useDeprecatedTag
             onClick={this.openDialog}
             color={Button.ButtonColor.gray}
-            text="Download parent letters"
+            text={i18n.downloadParentLetter()}
             icon="file-text"
           />
         </span>
@@ -80,8 +81,9 @@ export default class DownloadParentLetters extends Component {
           delayShow={500}
         >
           <div>
-            Download parent letters for all {this.props.numStudents} students in
-            this section
+            {i18n.downloadParentLetterTooltip({
+              numStudents: this.props.numStudents
+            })}
           </div>
         </ReactTooltip>
         <BaseDialog
@@ -90,23 +92,14 @@ export default class DownloadParentLetters extends Component {
           style={styles.dialog}
           handleClose={this.closeDialog}
         >
-          <h2>Parent letter for [section name]</h2>
-          <div>
-            Download a PDF letter to send to parents. The letter will include
-            information about Code.org and how Code.org protects student
-            privacy, plus tips for how parents can stay involved in their
-            student’s computer science education.
-          </div>
+          <h2>{i18n.parentLetterDialogTitle({sectionName: '*******'})}</h2>
+          <div>{i18n.parentLetterDialogDescription()}</div>
           <br />
           {LOGIN_TYPES_WITH_INDIVIDUAL_LETTERS_OPTION.includes(
             this.props.loginType
           ) && (
             <div>
-              <strong>
-                Do you want to include individual student login instructions
-                (username and password) in the letter so students can sign in at
-                home?
-              </strong>
+              <strong>{i18n.parentLetterDialogQuestion()}</strong>
               <div>
                 <input
                   style={styles.radioInput}
@@ -116,12 +109,9 @@ export default class DownloadParentLetters extends Component {
                   onChange={this.onRadioChange}
                 />
                 <label htmlFor={INDIVIDUAL} style={styles.radioLabel}>
-                  <strong>
-                    Yes, include individualized login instructions for each
-                    student
-                  </strong>
+                  <strong>{i18n.parentLetterDialogQ1Main()}</strong>
                   <br />
-                  This will download one letter per student as a .zip file.
+                  {i18n.parentLetterDialogQ1Detail()}
                 </label>
               </div>
               <div>
@@ -133,11 +123,9 @@ export default class DownloadParentLetters extends Component {
                   onChange={this.onRadioChange}
                 />
                 <label htmlFor={GENERIC} style={styles.radioLabel}>
-                  <strong>
-                    No, create one generic letter for all students
-                  </strong>
+                  <strong>{i18n.parentLetterDialogQ2Main()}</strong>
                   <br />
-                  This will download a single letter.
+                  {i18n.parentLetterDialogQ2Detail()}
                 </label>
               </div>
             </div>
@@ -145,7 +133,7 @@ export default class DownloadParentLetters extends Component {
           <DialogFooter rightAlign>
             <Button
               __useDeprecatedTag
-              text="Download letter"
+              text={i18n.parentLetterDialogButton()}
               onClick={this.download}
             />
           </DialogFooter>
