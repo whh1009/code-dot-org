afe91868193f30814d3311ac7bed37e775b765f6
Clare Constantine <clare@code.org>
2020-04-03 13:13:59 -0700

    don't offer individual letter option for sections we don't have login info for

--- apps/src/templates/manageStudents/DownloadParentLetters.jsx
+++ apps/src/templates/manageStudents/DownloadParentLetters.jsx
@@ -4,6 +4,7 @@ import Button from '@cdo/apps/templates/Button';
 import ReactTooltip from 'react-tooltip';
 import BaseDialog from '@cdo/apps/templates/BaseDialog';
 import DialogFooter from '@cdo/apps/templates/teacherDashboard/DialogFooter';
+import {SectionLoginType} from '@cdo/apps/util/sharedConstants';
 
 const INDIVIDUAL = 'individual';
 
@@ -26,6 +27,11 @@ const styles = {
     verticalAlign: 'middle'
   }
 };
+
+const LOGIN_TYPES_WITH_INDIVIDUAL_LETTERS_OPTION = [
+  SectionLoginType.word,
+  SectionLoginType.picture
+];
 
 export default class DownloadParentLetters extends Component {
   static propTypes = {
@@ -91,44 +97,50 @@ export default class DownloadParentLetters extends Component {
             student’s computer science education.
           </div>
           <br />
-          <div>
-            <strong>
-              Do you want to include individual student login instructions
-              (username and password) in the letter so students can sign in at
-              home?
-            </strong>
+          {LOGIN_TYPES_WITH_INDIVIDUAL_LETTERS_OPTION.includes(
+            this.props.loginType
+          ) && (
             <div>
-              <input
-                style={styles.radioInput}
-                type="radio"
-                id={INDIVIDUAL}
-                checked={this.props.letterType === INDIVIDUAL}
-                onChange={this.onRadioChange}
-              />
-              <label htmlFor={INDIVIDUAL} style={styles.radioLabel}>
-                <strong>
-                  Yes, include individualized login instructions for each
-                  student
-                </strong>
-                <br />
-                This will download one letter per student as a .zip file.
-              </label>
+              <strong>
+                Do you want to include individual student login instructions
+                (username and password) in the letter so students can sign in at
+                home?
+              </strong>
+              <div>
+                <input
+                  style={styles.radioInput}
+                  type="radio"
+                  id={INDIVIDUAL}
+                  checked={this.props.letterType === INDIVIDUAL}
+                  onChange={this.onRadioChange}
+                />
+                <label htmlFor={INDIVIDUAL} style={styles.radioLabel}>
+                  <strong>
+                    Yes, include individualized login instructions for each
+                    student
+                  </strong>
+                  <br />
+                  This will download one letter per student as a .zip file.
+                </label>
+              </div>
+              <div>
+                <input
+                  style={styles.radioInput}
+                  type="radio"
+                  id={GENERIC}
+                  checked={this.props.letterType === GENERIC}
+                  onChange={this.onRadioChange}
+                />
+                <label htmlFor={GENERIC} style={styles.radioLabel}>
+                  <strong>
+                    No, create one generic letter for all students
+                  </strong>
+                  <br />
+                  This will download a single letter.
+                </label>
+              </div>
             </div>
-            <div>
-              <input
-                style={styles.radioInput}
-                type="radio"
-                id={GENERIC}
-                checked={this.props.letterType === GENERIC}
-                onChange={this.onRadioChange}
-              />
-              <label htmlFor={GENERIC} style={styles.radioLabel}>
-                <strong>No, create one generic letter for all students</strong>
-                <br />
-                This will download a single letter.
-              </label>
-            </div>
-          </div>
+          )}
           <DialogFooter rightAlign>
             <Button
               __useDeprecatedTag
