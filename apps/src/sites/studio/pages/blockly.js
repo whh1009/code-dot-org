9d1a5af7eaac487286dd55275825f83dbd85d1c7
Anjali <anjali@code.org>
2020-05-11 14:47:13 -0700

    re-add BlockValueType

--- apps/src/sites/studio/pages/blockly.js
+++ apps/src/sites/studio/pages/blockly.js
@@ -4,4 +4,32 @@ import 'blockly/blocks';
 import 'blockly/javascript';
 Blockly.setLocale(locale);
 
+Blockly.BlockValueType = {
+  NONE: 'None', // Typically as a connection/input check means "accepts any type"
+  STRING: 'String',
+  NUMBER: 'Number',
+  IMAGE: 'Image',
+  BOOLEAN: 'Boolean',
+  FUNCTION: 'Function',
+  COLOUR: 'Colour',
+  ARRAY: 'Array',
+
+  // p5.play Sprite
+  SPRITE: 'Sprite',
+
+  /**
+   * {Object} Behavior
+   * {function} Behavior.func
+   * {Array} Behavior.extraArgs
+   */
+  BEHAVIOR: 'Behavior',
+
+  /**
+   * {Object} Location
+   * {number} Location.x
+   * {number} Location.y
+   */
+  LOCATION: 'Location'
+};
+
 window.Blockly = Blockly;
