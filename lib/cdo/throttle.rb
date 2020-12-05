6d37bf3ae8f4f6053aaea00b1638bfc150be6cdb
Madelyn Kasula <maddie.kasula@code.org>
2020-12-04 18:01:05 -0800

    More WIP

--- lib/cdo/throttle.rb
+++ lib/cdo/throttle.rb
@@ -3,15 +3,16 @@ require 'cdo/shared_cache'
 module Cdo
   module Throttle
     # TODO: what should this value be?
-    THROTTLE_TIME = 10 # seconds
+    THROTTLE_TIME = 10.seconds.freeze
     CACHE_PREFIX = "cdo_throttle/".freeze
 
-    # @param [String] cache_key - Unique key to throttle on.
+    # @param [String] id - Unique identifier to throttle on.
     # @param [Integer] limit - Number of requests allowed over period.
     # @param [Integer] period - Period of time in seconds.
     # @returns [Boolean] Whether or not the request should be throttled.
-    def self.throttle(cache_key, limit, period)
-      full_key = CACHE_PREFIX + cache_key.to_s
+    # Once an ID has been throttled, it will stay throttled for Cdo::Throttle::THROTTLE_TIME.
+    def self.throttle(id, limit, period)
+      full_key = CACHE_PREFIX + id.to_s
       value = CDO.shared_cache.read(full_key) || empty_value
       now = Time.now.utc
       value[:request_timestamps] << now
